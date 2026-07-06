843f5e3f44cfa88fa49f3c8b6315904ff0e570a4
asif-ahmed-shaik <asifahmedshaik18@gmail.com>
2026-07-06 12:38:26 +0530

    Implement search student feature

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-student = []
-
-print("=" * 45)
-print("     STUDENT MANAGEMENT SYSTEM")
-print("=" * 45)
-
-print("1. Add Student")
-print("2. View Students")
-print("3. Search Student")
-print("4. Update Student")
-print("5. Delete Student")
-print("6. Exit")
-
-choice = input("\nEnter your choice: ")
-
-print(f"\nYou selected option {choice}")
+students = []
 
 def add_student():
     print("\n--- Add Student ---")
@@ -34,9 +19,77 @@ def add_student():
 
     print("\n Student added successfully!")
 
-    print(f"\nYou selected option {choice}")
+def view_students():
+
+    print("\n--- Student Records ---")
+
+    if len(students) == 0:
+        print("No students found.")
+        return
+
+    for student in students:
+        print("----------------------------")
+        print(f"ID         : {student['id']}")
+        print(f"Name       : {student['name']}")
+        print(f"Age        : {student['age']}")
+        print(f"Department : {student['department']}")
+
+    print("----------------------------")
+
+def search_student():
+
+    print("\n--- Search Student ---")
+
+    search_id = input("Enter Student ID: ")
+
+    found = False
+
+    for student in students:
+
+        if student["id"] == search_id:
+
+            print("\nStudent Found!")
+            print("----------------------------")
+            print(f"ID         : {student['id']}")
+            print(f"Name       : {student['name']}")
+            print(f"Age        : {student['age']}")
+            print(f"Department : {student['department']}")
+            print("----------------------------")
+
+            found = True
+            break
+
+    if not found:
+        print("\n Student not found.")
+
+
+while True:
+
+    print("\n" + "=" * 45)
+    print("     STUDENT MANAGEMENT SYSTEM")
+    print("=" * 45)
+
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Update Student")
+    print("5. Delete Student")
+    print("6. Exit")
+
+    choice = input("\nEnter your choice: ")
 
     if choice == "1":
         add_student()
+
+    elif choice == "2":
+        view_students()
+
+    elif choice == "3":
+        search_student()
+
+    elif choice == "6":
+        print("\nThank you for using Student Management System!")
+        break
+
     else:
-        print("This option is not implemented yet.")
+        print("\n⚠️ This feature is coming soon.")
